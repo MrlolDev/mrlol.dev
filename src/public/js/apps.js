279d1b8f56d1445e23a268860d4ebb562a1f63e3
Mrlolpro <leonardoollero14@gmail.com>
2021-11-15 16:31:44 +0100

    1.1.1

--- src/public/js/apps.js
+++ src/public/js/apps.js
@@ -53,14 +53,11 @@ document.addEventListener('click', function(event) {
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if(elmnt.tag =='input') return console.log('test')
   if (document.getElementById(elmnt.id + "header")) {
     // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
+  } 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
